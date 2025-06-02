8e2661c3e61b067975a45d15dd454a76342caa85
naqeebullah19 <135517103+naqeebullah19@users.noreply.github.com>
2025-06-03 00:37:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ biddict={}
 a=input("what is your name?")
 a1=int(input("whats your bid?"))
 
-biddict["a"]=a1
-
+biddict[a]=a1
+values=biddict.values()
+counter=1
 others=input("are there any other bidders?")
 while others=='yes':
-    print("\n"*100)
-    b=input("what is your name?")
-    b1=int(input("whats your bid?"))
+    if others=='yes':
+        
+        print("\n"*100)
+        b=input("what is your name?")
+        b1=int(input("whats your bid?"))
+        biddict[b]=b1
+        others=input("are there any other bidders?")
+        counter+=1
+    else:
+        break
+    
+sorted_dict = dict(sorted(biddict.items()))
+first_key = next(iter(sorted_dict))
+print(f"{first_key} is the winner of the auction with a bid of {sorted_dict[first_key]}")
+    
